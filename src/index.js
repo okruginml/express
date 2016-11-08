e612f597b2a50f85a5bfe9a3814ce2ed7a587193
okruginml <okruginml@ya.ru>
2016-11-09 00:14:08 +0300

    task2B

--- src/index.js
+++ src/index.js
@@ -14,6 +14,29 @@ app.get('/task2A', (req, res) => {
   res.send(sum.toString());
 });
 
+app.get('/task2B', (req, res) => {
+  const fname = req.query.fullname;
+  var fio = fname.split(' ');
+  var answer = 'Invalid fullname';
+  const nwords = fio.length; 
+  if (nwords == 3) {
+  	answer = (fio[2] + ' ' + fio[0].substring(0,1) + '. ' + fio[1].substring(0,1) + '.');
+  }
+  if (nwords == 2) {
+  	answer = (fio[1] + ' ' + fio[0].substring(0,1) + '.');
+  }
+  if (nwords == 1) {
+  	answer = (fio[0]);
+  }
+  if (!!!fname) {
+  	answer = 'Invalid fullname';
+  }
+  if (fname.match('[0-9]') != null) {
+  	answer = 'Invalid fullname';
+  }
+  res.send(answer);
+});
+
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
 });
